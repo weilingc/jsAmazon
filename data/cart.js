9c2884792b94f5369691ab3ea3593b4103a2d9db
weilingc <madeintw66@gmail.com>
2023-08-04 21:15:24 +0800

    add localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,13 @@
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 1
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 2
-  }
-];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+  cart = []
+}
+
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId) {
   let matchItem;
@@ -25,17 +25,23 @@ export function addToCart(productId) {
       quantity: 1
     });
   }
+  saveToStorage();
+  console.log(cart)
+  console.log(localStorage)
 }
 
 export function removeFromCart(productId) {
   const newCart = [];
+
   cart.forEach((cartItem) => {
     if (cartItem.productId !== productId) {
       newCart.push(cartItem)
     }
-
-    cart = newCart;
   })
+
+  cart = newCart;
+
+  saveToStorage();
 }
 
 // function upd
